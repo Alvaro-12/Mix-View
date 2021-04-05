39759734acde83aa779993953723f944be525da9
Alex-Hub-cyber <gl0430032020@unab.edu.sv>
2021-04-05 18:14:12 -0500

    Renombrado Registro
    .

--- Mix-View/Vista/FrmReg.Designer.cs
+++ Mix-View/Vista/FrmReg.Designer.cs
@@ -31,101 +31,123 @@ namespace Mix_View.Vista
         {
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.Gmail = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.RdM = new System.Windows.Forms.RadioButton();
+            this.Rdf = new System.Windows.Forms.RadioButton();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.TxtNombre = new System.Windows.Forms.TextBox();
+            this.TxtGmail = new System.Windows.Forms.TextBox();
+            this.TxtEdad = new System.Windows.Forms.TextBox();
+            this.TxtPass = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(42, 65);
+            this.label1.Location = new System.Drawing.Point(26, 62);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(98, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Nombre de Usuario";
             // 
-            // label2
+            // Gmail
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(42, 136);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.Gmail.AutoSize = true;
+            this.Gmail.Location = new System.Drawing.Point(42, 136);
+            this.Gmail.Name = "Gmail";
+            this.Gmail.Size = new System.Drawing.Size(33, 13);
+            this.Gmail.TabIndex = 1;
+            this.Gmail.Text = "Gmail";
             // 
-            // label3
+            // Label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(42, 205);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(42, 205);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(32, 13);
+            this.Label3.TabIndex = 2;
+            this.Label3.Text = "Edad";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(42, 271);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(53, 13);
             this.label4.TabIndex = 3;
-            this.label4.Text = "label4";
+            this.label4.Text = "Password";
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.radioButton2);
-            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.RdM);
+            this.groupBox1.Controls.Add(this.Rdf);
             this.groupBox1.Location = new System.Drawing.Point(45, 327);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(288, 100);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "Genero";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // RdM
+            // 
+            this.RdM.AutoSize = true;
+            this.RdM.Location = new System.Drawing.Point(160, 48);
+            this.RdM.Name = "RdM";
+            this.RdM.Size = new System.Drawing.Size(73, 17);
+            this.RdM.TabIndex = 1;
+            this.RdM.TabStop = true;
+            this.RdM.Text = "Masculino";
+            this.RdM.UseVisualStyleBackColor = true;
+            // 
+            // Rdf
+            // 
+            this.Rdf.AutoSize = true;
+            this.Rdf.Location = new System.Drawing.Point(48, 48);
+            this.Rdf.Name = "Rdf";
+            this.Rdf.Size = new System.Drawing.Size(71, 17);
+            this.Rdf.TabIndex = 0;
+            this.Rdf.TabStop = true;
+            this.Rdf.Text = "Femenino";
+            this.Rdf.UseVisualStyleBackColor = true;
             // 
             // contextMenuStrip1
             // 
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
-            // textBox1
+            // TxtNombre
             // 
-            this.textBox1.Location = new System.Drawing.Point(106, 65);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(227, 20);
-            this.textBox1.TabIndex = 6;
+            this.TxtNombre.Location = new System.Drawing.Point(146, 59);
+            this.TxtNombre.Name = "TxtNombre";
+            this.TxtNombre.Size = new System.Drawing.Size(227, 20);
+            this.TxtNombre.TabIndex = 6;
             // 
-            // textBox2
+            // TxtGmail
             // 
-            this.textBox2.Location = new System.Drawing.Point(106, 133);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(227, 20);
-            this.textBox2.TabIndex = 7;
+            this.TxtGmail.Location = new System.Drawing.Point(146, 136);
+            this.TxtGmail.Name = "TxtGmail";
+            this.TxtGmail.Size = new System.Drawing.Size(227, 20);
+            this.TxtGmail.TabIndex = 7;
             // 
-            // textBox3
+            // TxtEdad
             // 
-            this.textBox3.Location = new System.Drawing.Point(106, 198);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(227, 20);
-            this.textBox3.TabIndex = 8;
+            this.TxtEdad.Location = new System.Drawing.Point(146, 202);
+            this.TxtEdad.Name = "TxtEdad";
+            this.TxtEdad.Size = new System.Drawing.Size(227, 20);
+            this.TxtEdad.TabIndex = 8;
             // 
-            // textBox4
+            // TxtPass
             // 
-            this.textBox4.Location = new System.Drawing.Point(106, 271);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(227, 20);
-            this.textBox4.TabIndex = 9;
+            this.TxtPass.Location = new System.Drawing.Point(146, 271);
+            this.TxtPass.Name = "TxtPass";
+            this.TxtPass.Size = new System.Drawing.Size(227, 20);
+            this.TxtPass.TabIndex = 9;
             // 
             // button1
             // 
@@ -133,45 +155,23 @@ namespace Mix_View.Vista
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 10;
-            this.button1.Text = "button1";
+            this.button1.Text = "Registrarse";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(48, 48);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(85, 17);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "radioButton1";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(160, 48);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(85, 17);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "radioButton2";
-            this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // FrmReg
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(380, 507);
+            this.ClientSize = new System.Drawing.Size(417, 507);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TxtPass);
+            this.Controls.Add(this.TxtEdad);
+            this.Controls.Add(this.TxtGmail);
+            this.Controls.Add(this.TxtNombre);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Gmail);
             this.Controls.Add(this.label1);
             this.Name = "FrmReg";
             this.Text = "FrmReg";
@@ -185,17 +185,17 @@ namespace Mix_View.Vista
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Gmail;
+        private System.Windows.Forms.Label Label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox TxtNombre;
+        private System.Windows.Forms.TextBox TxtGmail;
+        private System.Windows.Forms.TextBox TxtEdad;
+        private System.Windows.Forms.TextBox TxtPass;
+        private System.Windows.Forms.RadioButton RdM;
+        private System.Windows.Forms.RadioButton Rdf;
         private System.Windows.Forms.Button button1;
     }
 }
