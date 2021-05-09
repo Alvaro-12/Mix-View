a07d4d3d5e2a4c517d1286fb23111c3c4ff905ab
nelson calles <cc0324032019@unab.edu.sv>
2021-05-09 15:56:48 -0300

    repro

---
+++ Mix-View/DAO/ClsReproductor.cs
@@ -0,0 +1,19 @@
+﻿using Mix_View.Vista;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mix_View.DAO
+{
+    class ClsReproductor
+    {
+        public void Reproducir(string url)
+        {
+            FrmReproductor video = new FrmReproductor();
+            video.video(url);
+            video.Show();
+        }
+    }
+}
