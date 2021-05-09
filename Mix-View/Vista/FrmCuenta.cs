5b39c267ea19b6e778bf500734165015287b4c26
nelson calles <cc0324032019@unab.edu.sv>
2021-05-09 16:08:28 -0300

    cuenta

---
+++ Mix-View/Vista/FrmCuenta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mix_View.Vista
+{
+    public partial class FrmCuenta : Form
+    {
+        public FrmCuenta()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
