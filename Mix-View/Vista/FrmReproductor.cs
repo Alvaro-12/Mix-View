df403cae909cfde91c2d5084f788d012ef5e71cb
Alex-Hub-cyber <gl0430032020@unab.edu.sv>
2021-05-09 15:32:31 -0500

    Reproductor
    Reproductor

---
+++ Mix-View/Vista/FrmReproductor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mix_View.Vista
+{
+    public partial class FrmReproductor : Form
+    {
+        public FrmReproductor()
+        {
+            InitializeComponent();
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+            
+           
+        }
+    }
+}
